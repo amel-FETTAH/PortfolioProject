f4b58096e7d058fd060d00885f15be2bcb2d26fa
amel-FETTAH <amelfettah43@gmail.com>
2025-10-15 13:31:11 +0200

    Fix connexion MySQL cloud - ajout du port

--- backend/db.js
+++ backend/db.js
@@ -4,16 +4,17 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
+   host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306, // ✅ port dynamique (cloud ou local)
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || 'portfolio',
   waitForConnections: true,
   connectionLimit: 10,
 
-  // ✅ Ajout essentiel pour les serveurs MySQL Cloud
+  // ✅ Option SSL (utile pour MySQL Cloud)
   ssl: {
-    rejectUnauthorized: true, // sécurité recommandée
+    rejectUnauthorized: true, // garde la connexion sécurisée
   },
 });
 
